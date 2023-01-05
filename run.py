eb4de656937dc8824cec3ceedd7a3cc9fab665b3
fatimaqais <fatima_qais@hotmail.com>
2023-01-05 16:52:23 +0000

    Add a list of words and random word picker

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -8,7 +7,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -20,3 +19,21 @@ score = SHEET.worksheet('userscores')
 data = score.get_all_values()
 
 print(data)
+
+words = ['sad', 'happy', 'excited', 'lucky', 'swimming', 'jump', 'running',
+         'climbing', 'movie', 'music', 'dancing', 'apple', 'banana', 'oranges',
+         'mango', 'pineapple', 'guava', 'lemon', 'watermelon', 'strawberry',
+         'building', 'mountain', 'river', 'trees', 'tiger', 'lion', 'elephant']
+
+
+def get_random_words():
+    """
+    Picks a random word from the words list.
+    Displays it to user in uppercase letters.
+    """
+    random_word = random.choice(words).upper()
+    print(random_word)
+    return random_word.upper()
+
+
+get_random_words()
